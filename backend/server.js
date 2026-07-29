8a2562ee78657545784bd35ad29531f9f6f4ce4f
Pranav D Pandit <pranav2000pandit@gmail.com>
2026-07-29 07:18:12 +0000

    Update server.js
    erripuku pranav

--- backend/server.js
+++ backend/server.js
@@ -17,12 +17,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(express.json());
 app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
+app.use(express.json());
+
 // app.options("*", cors());
 
 // Testing API
