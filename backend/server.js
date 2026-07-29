e957f06f0562c573bc50eb40370cfa6851b8ef82
Pranav D Pandit <pranav2000pandit@gmail.com>
2026-07-29 07:10:29 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
-app.options("*", cors());
+// app.options("*", cors());
 
 // Testing API
 app.get("/", (req, res) => {
